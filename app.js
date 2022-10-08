e2cfb212e5113b89d6fb6373c24d45a4f97cb41d
Jordan F <jordanfear1989@gmail.com>
2022-10-08 12:00:00 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT;
+
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error(err))
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
